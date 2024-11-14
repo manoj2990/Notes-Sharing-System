80d2ed91dcc9e44b7d997689e48ebcdbc05cca73
manoj2990 <manojmeena2990@gmail.com>
2024-11-15 01:19:40 +0530

    allowed_formats

--- Backend/utilityFiles/cloudinaryUtils.js
+++ Backend/utilityFiles/cloudinaryUtils.js
@@ -86,13 +86,14 @@
 
 const { cloudinary } = require("../config/cloudinary");
 
-// Function to upload file to Cloudinary
+
 const uploadOnCloudinary = (fileBuffer) => {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload_stream(
       {
-        resource_type: 'raw', // Automatically detect file type (PDF, DOCX, etc.)
+        resource_type: 'raw', 
         folder: 'NotesPdf',
+        allowed_formats: ['pdf', 'doc', 'docx'],
         
       },
       (error, result) => {
@@ -104,11 +105,11 @@ const uploadOnCloudinary = (fileBuffer) => {
           resolve(result);
         }
       }
-    ).end(fileBuffer); // Send the file buffer to Cloudinary
+    ).end(fileBuffer); 
   });
 };
 
-// Function to delete file from Cloudinary
+
 const deleteFromCloudinary = async (publicId) => {
   console.log("Public ID to delete:", publicId);
   try {
@@ -117,7 +118,7 @@ const deleteFromCloudinary = async (publicId) => {
       return false;
     }
 
-    // Use 'raw' only for non-image, non-video files; adjust based on actual file type stored
+  
     const response = await cloudinary.uploader.destroy(publicId, { resource_type: 'raw' });
     console.log("Cloudinary delete response:", response);
 
@@ -133,15 +134,15 @@ const deleteFromCloudinary = async (publicId) => {
   }
 };
 
-// Helper function to extract Public ID from URL
+
 const extractPublicIdFromUrl = (url) => {
   try {
     const parts = url.split('/');
-    const fileName = parts[parts.length - 1].split('.')[0]; // Remove file extension
+    const fileName = parts[parts.length - 1].split('.')[0]; 
     const fileFolder = parts[parts.length - 2];
     const publicId = `${fileFolder}/${fileName}`;
     
-    console.log("Extracted public ID:", publicId); // Log the extracted public ID for debugging
+    console.log("Extracted public ID:", publicId);
     return publicId;
   } catch (error) {
     console.error("Error extracting public ID from URL:", error.message);

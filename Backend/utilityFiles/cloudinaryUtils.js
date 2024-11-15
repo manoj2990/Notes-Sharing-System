13e2b123aa8a7fbb4287a87cc55417c595e42471
manoj2990 <manojmeena2990@gmail.com>
2024-11-15 12:27:03 +0530

    test uploadOnCloudinary

--- Backend/utilityFiles/cloudinaryUtils.js
+++ Backend/utilityFiles/cloudinaryUtils.js
@@ -91,7 +91,7 @@ const uploadOnCloudinary = (fileBuffer) => {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload_stream(
       {
-        resource_type: 'auto', 
+        resource_type: 'raw', 
         folder: 'NotesPdf',
         allowed_formats: ['pdf', 'doc', 'docx'],
         
